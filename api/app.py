5b9d00774a6565ca3b717538e03bb735e6733b16
A-Gui <sa6549872000@gmail.com>
2026-02-02 10:04:39 +0800

    20260202

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-if os.getenv('FLASK_ENV') == 'development':
+if os.getenv('FLASK_ENV') == 'production':
     from dotenv import load_dotenv
     load_dotenv()
     
